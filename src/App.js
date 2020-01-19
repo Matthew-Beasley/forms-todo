3d670fc68b0d1816bfe0688077b13699d34d6272
conbec <56614504+conbec@users.noreply.github.com>
2020-01-18 22:20:49 -0800

    two bugs, view all and line through, instrumantation for Preston

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [todos, create] = useState([]);
+  const [text, setText] = useState('');
+  const [hidden, setButtonText] = useState('hide all');
+
+  const createToDo = (event) => {
+    const newToDo = { text: text, isComplete: false };
+    create([...todos, newToDo]);
+    setText('');
+  }
+
+
+  const toggleDone = ({target}) => {
+
+    todos.forEach(todo => {
+
+      if (todo.text === target.innerText && !target.classList.contains('completed')) {
+        target.classList.add('completed');
+        todo.isComplete = true;
+        console.log('add ', target)
+      }
+      else if (todo.text === target.innerText && target.classList.contains('completed')) {
+        target.classList.remove('completed');
+        todo.isComplete = false;
+        console.log('remove ', target)
+      }
+    })
+  }
+
+
+  const hideComplete = ({target}) => {
+    target.classList.toggle('hidden');
+    target.classList.contains('hidden') ? setButtonText('view all') : setButtonText('hide all'); 
+  
+    const done = todos.filter(todo => {
+      if (todo.isComplete === false) {
+        return todo;
+      }
+    })
+
+    console.log(hidden)
+    console.log('todos is ', todos)
+    console.log('done is ', done);
+    
+    if (hidden === 'hide all') {
+      create([...done]);
+    } 
+    else {
+      create([...todos]); // why doesn't this work? and why are not completes getting class of completed
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <form onSubmit={(event) => { event.preventDefault() }}>
+          <input type="text" value={text} onChange={(event) => setText(event.target.value)}></input>
+          <button onClick={(event) => { createToDo(event) }}>Create</button>
+          <button className="hide-button" onClick={(e) => {hideComplete(e)}}>{hidden}</button>
+        </form>      
+      </div>
+      <div>
+        <ul>
+          {todos.map((todo, idx) => {
+            return (
+              <li key={idx} onClick={event => {toggleDone(event)}}>{todo.text}</li>
+            )
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
