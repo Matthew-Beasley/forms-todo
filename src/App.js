c8c21338b0e6ec374415f32715247923122bc6c1
conbec <56614504+conbec@users.noreply.github.com>
2020-01-20 08:54:54 -0800

    prestons fixes

--- src/App.js
+++ src/App.js
@@ -3,54 +3,37 @@ import './App.css';
 
 function App() {
 
-  const [todos, create] = useState([]);
+  const [todos, setTodos] = useState([]);
   const [text, setText] = useState('');
-  const [hidden, setButtonText] = useState('hide all');
+  const [hidden, setButtonText] = useState('view all');
 
-  const createToDo = (event) => {
+  const createToDo = () => {
     const newToDo = { text: text, isComplete: false };
-    create([...todos, newToDo]);
+    setTodos([...todos, newToDo]);
     setText('');
   }
 
 
-  const toggleDone = ({target}) => {
+  const toggleDone = (clickedTodo, todoIndex) => {
+console.log(clickedTodo)
+    const dos = todos.map((todo, idx) => {
 
-    todos.forEach(todo => {
-
-      if (todo.text === target.innerText && !target.classList.contains('completed')) {
-        target.classList.add('completed');
-        todo.isComplete = true;
-        console.log('add ', target)
+      if (todoIndex === idx) {
+        todo.isComplete = !todo.isComplete;
       }
-      else if (todo.text === target.innerText && target.classList.contains('completed')) {
-        target.classList.remove('completed');
-        todo.isComplete = false;
-        console.log('remove ', target)
-      }
+      return todo;
     })
+
+    setTodos(dos);
   }
 
 
-  const hideComplete = ({target}) => {
-    target.classList.toggle('hidden');
-    target.classList.contains('hidden') ? setButtonText('view all') : setButtonText('hide all'); 
-  
-    const done = todos.filter(todo => {
-      if (todo.isComplete === false) {
-        return todo;
-      }
-    })
-
-    console.log(hidden)
-    console.log('todos is ', todos)
-    console.log('done is ', done);
-    
-    if (hidden === 'hide all') {
-      create([...done]);
-    } 
+  const hideComplete = ({ target }) => {
+    if (hidden === 'view all') {
+      setButtonText('hide all')
+    }
     else {
-      create([...todos]); // why doesn't this work? and why are not completes getting class of completed
+      setButtonText('view all')
     }
   }
 
@@ -60,7 +43,7 @@ function App() {
       <div>
         <form onSubmit={(event) => { event.preventDefault() }}>
           <input type="text" value={text} onChange={(event) => setText(event.target.value)}></input>
-          <button onClick={(event) => { createToDo(event) }}>Create</button>
+          <button onClick={createToDo}>Create</button>
           <button className="hide-button" onClick={(e) => {hideComplete(e)}}>{hidden}</button>
         </form>      
       </div>
@@ -68,7 +51,8 @@ function App() {
         <ul>
           {todos.map((todo, idx) => {
             return (
-              <li key={idx} onClick={event => {toggleDone(event)}}>{todo.text}</li>
+              todo.isComplete  && hidden === 'hide all' ? '': 
+              <li key={idx} className={todo.isComplete ? 'completed' : ''} onClick={event => {toggleDone(todo, idx)}}>{todo.text}</li>
             )
           })}
         </ul>
